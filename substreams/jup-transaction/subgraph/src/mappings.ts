80e42f451259c6bcd78e64147b97d06a9aafbf1e
SohZHong <sohzhehong09@gmail.com>
2025-03-08 13:46:11 +0800

    finalized jupiter transaction substreams and subgraph

--- substreams/jup-transaction/subgraph/src/mappings.ts
+++ substreams/jup-transaction/subgraph/src/mappings.ts
@@ -1,15 +1,85 @@
-import { Protobuf } from "as-proto/assembly";
-import { MyData as protoMyData } from "./pb/mydata/v1/MyData";
-import { MyEntity } from "../generated/schema";
-import { BigInt, log, crypto, Bytes} from "@graphprotocol/graph-ts";
+import { Protobuf } from 'as-proto/assembly';
+import { MyData as protoMyData } from './pb/mydata/v1/MyData';
+import {
+  JupTransaction,
+  TokenBalance,
+  UiTokenAmount,
+} from '../generated/schema';
+import { BigInt, Bytes } from '@graphprotocol/graph-ts';
+
+// Decode JUP token address from Base58
+const JUP_TOKEN: string = 'JUP6LkbZbjS1jKKwapdHNy74zcZ3tLUZoi5QNyVTaV4';
 
 export function handleTriggers(bytes: Uint8Array): void {
   const input = Protobuf.decode<protoMyData>(bytes, protoMyData.decode);
-  
-  // No ID field has been found in the proto input...
-  // The input has been hashed to create a unique ID, replace it with the field you want to use as ID
-  const inputHash = crypto.keccak256(Bytes.fromUint8Array(bytes)).toHexString();
-  let entity = new MyEntity(inputHash);
 
-  entity.save();
+  for (var i = 0; i < input.transactions.length; i++) {
+    const transaction = input.transactions[i].transaction!;
+    const meta = input.transactions[i].meta!;
+
+    // Create JupTransaction entity
+    let entity = new JupTransaction(
+      Bytes.fromUint8Array(transaction.signatures[0]).toBase58()
+    );
+
+    // Store user
+    entity.user = Bytes.fromUint8Array(
+      transaction.message!.accountKeys[0]
+    ).toBase58();
+
+    const accountKeys = transaction.message!.accountKeys.map<string>((key) =>
+      Bytes.fromUint8Array(key).toBase58()
+    );
+
+    // Handle Pre-Token Balances
+    for (let j = 0; j < meta.preTokenBalances.length; j++) {
+      const balance = meta.preTokenBalances[j];
+
+      let tokenBalance = new TokenBalance(
+        entity.id + '-pre-' + j.toString() // Unique ID
+      );
+      tokenBalance.preTransaction = entity.id;
+      tokenBalance.accountIndex = BigInt.fromU32(balance.accountIndex);
+      tokenBalance.mint = balance.mint;
+      tokenBalance.owner = balance.owner;
+      tokenBalance.programId = balance.programId;
+
+      // Create UiTokenAmount entity
+      let uiAmountEntity = new UiTokenAmount(tokenBalance.id);
+      uiAmountEntity.tokenBalance = tokenBalance.id;
+
+      uiAmountEntity.decimals = BigInt.fromU32(balance.uiTokenAmount!.decimals);
+      uiAmountEntity.amount = balance.uiTokenAmount!.amount;
+      uiAmountEntity.uiAmountString = balance.uiTokenAmount!.uiAmountString;
+
+      uiAmountEntity.save();
+      tokenBalance.save();
+    }
+
+    // Handle Post-Token Balances
+    for (let k = 0; k < meta.postTokenBalances.length; k++) {
+      const balance = meta.postTokenBalances[k];
+
+      let tokenBalance = new TokenBalance(
+        entity.id + '-post-' + k.toString() // Unique ID
+      );
+      tokenBalance.postTransaction = entity.id;
+      tokenBalance.accountIndex = BigInt.fromU32(balance.accountIndex);
+      tokenBalance.mint = balance.mint;
+      tokenBalance.owner = balance.owner;
+      tokenBalance.programId = balance.programId;
+      // Create UiTokenAmount entity
+      let uiAmountEntity = new UiTokenAmount(tokenBalance.id);
+      uiAmountEntity.tokenBalance = tokenBalance.id;
+
+      uiAmountEntity.decimals = BigInt.fromU32(balance.uiTokenAmount!.decimals);
+      uiAmountEntity.amount = balance.uiTokenAmount!.amount;
+      uiAmountEntity.uiAmountString = balance.uiTokenAmount!.uiAmountString;
+
+      uiAmountEntity.save();
+      tokenBalance.save();
+    }
+
+    entity.save();
+  }
 }
